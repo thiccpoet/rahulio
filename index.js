3117e07cc8f2248645df3434115595d2cc14f19e
thiccpoet <65043687+thiccpoet@users.noreply.github.com>
2020-05-10 22:34:11 -0400

    1.0.5

--- index.js
+++ index.js
@@ -28,6 +28,90 @@ client.on('message', message => {
 
 
 	if(command === 'test') message.channel.send('Rahulio ' + version + ' is online and functioning!');
+
+	else if (command.startsWith(`ouija`)){
+		if(args.length > 1) {
+			const yn = [`yes`,`no`,`maybe`,`unknown`,`unlikely`,`very likely`,`doubtful`];
+			const what = [`vanilla`,`what?`,`anal`,`unknown`,`everything is meaningless`,`nothing`];
+			const why = [`no idea`,`carnal urges`,`deformities at birth`,`the ways of life`,`she doesn't love you`,`fortune`,`lack of initative`];
+			const when = [`never`,`soon`,`when pigs fly`,`unknown`,`it's already happened`,`right now`,`in an eternity`];
+			const where = [`in hell`,`up your ass and to the left`,`nowhere`,`somewhere`,`unknown`,`New York`];
+			const who = [`you`,`your mother`,`your neighbor`,`unknown`,`a hooker`,`a nobody`,`a rich old man with a wrinkly penis`];
+			const how = [`no idea`,`somehow`,`raw`,`it's impossible`,`with love`,`gotta believe`];
+			if (args[0].toLowerCase() === `is` || args[0].toLowerCase() === `are` || args[0].toLowerCase() === `am` || args[0].toLowerCase() === `will`){
+				if(message.substring(message.length-1) === `?`){
+					message.channel.send(`Question: ` + message.substring(8));	
+					message.channel.send(`Answer: ` + yn[Math.floor(Math.random() * yn.length)] + `!`);
+				}
+				else{
+					message.channel.send(`Question: ` + message.substring(8) + `?`);	
+					message.channel.send(`Answer: ` + yn[Math.floor(Math.random() * yn.length)] + `!`);
+				}
+			}
+			else if (args[0].toLowerCase() === `what`){
+				if(message.substring(message.length-1) === `?`){
+					message.channel.send(`Question: ` + message.substring(8));	
+					message.channel.send(`Answer: ` + what[Math.floor(Math.random() * what.length)] + `!`);
+				}
+				else{
+					message.channel.send(`Question: ` + message.substring(8) + `?`);	
+					message.channel.send(`Answer: ` + what[Math.floor(Math.random() * what.length)] + `!`);
+				}
+			}
+			else if (args[0].toLowerCase() === `why`){
+				if(message.substring(message.length-1) === `?`){
+					message.channel.send(`Question: ` + message.substring(8));	
+					message.channel.send(`Answer: ` + why[Math.floor(Math.random() * why.length)] + `!`);
+				}
+				else{
+					message.channel.send(`Question: ` + message.substring(8) + `?`);	
+					message.channel.send(`Answer: ` + why[Math.floor(Math.random() * why.length)] + `!`);
+				}
+			}
+			else if (args[0].toLowerCase() === `when`){
+				if(message.substring(message.length-1) === `?`){
+					message.channel.send(`Question: ` + message.substring(8));
+					message.channel.send(`Answer: ` + when[Math.floor(Math.random() * when.length)] + `!`);	
+				}
+				else{
+					message.channel.send(`Question: ` + message.substring(8) + `?`);	
+					message.channel.send(`Answer: ` + when[Math.floor(Math.random() * when.length)] + `!`);
+				}
+			}
+			else if (args[0].toLowerCase() === `where`){
+				if(message.substring(message.length-1) === `?`){
+					message.channel.send(`Question: ` + message.substring(8));	
+					message.channel.send(`Answer: ` + where[Math.floor(Math.random() * where.length)] + `!`);
+				}
+				else{
+					message.channel.send(`Question: ` + message.substring(8) + `?`);	
+					message.channel.send(`Answer: ` + where[Math.floor(Math.random() * where.length)] + `!`);
+				}
+			}
+			else if (args[0].toLowerCase() === `who`){
+				if(message.substring(message.length-1) === `?`){
+					message.channel.send(`Question: ` + message.substring(8));	
+					message.channel.send(`Answer: ` + who[Math.floor(Math.random() * who.length)] + `!`);
+				}
+				else{
+					message.channel.send(`Question: ` + message.substring(8) + `?`);	
+					message.channel.send(`Answer: ` + who[Math.floor(Math.random() * who.length)] + `!`);
+				}
+			}
+			else if (args[0].toLowerCase() === `how`){
+				if(message.substring(message.length-1) === `?`){
+					message.channel.send(`Question: ` + message.substring(8));	
+					message.channel.send(`Answer: ` + how[Math.floor(Math.random() * how.length)] + `!`);
+				}
+				else{
+					message.channel.send(`Question: ` + message.substring(8) + `?`);	
+					message.channel.send(`Answer: ` + how[Math.floor(Math.random() * how.length)] + `!`);
+				}
+			}
+			else {message.channel.send(`Please begin your question with [is/are/am/will/what/why/when/where/who/how]!`)};
+		}
+		else {message.channel.send(`Please ask a valid question!`)}
+	}
 	
 
 	else if (command === 'coin'){
@@ -97,7 +181,7 @@ client.on('message', message => {
 			.setDescription('Thank you for calling the r$help command!')
 			.setThumbnail('https://imgur.com/12fKOZZ.jpg')
 			.addFields(
-				{ name: 'Popular Commands', value: 'r$carti, r$server, r$test, r$coin\n'},
+				{ name: 'Popular Commands', value: 'r$carti, r$ouija, r$server, r$test, r$coin\n'},
 			//	{ name: '\u200B', value: '\u200B' },
 			//	{ name: 'Inline field title', value: 'Some value here', inline: true },
 			//	{ name: 'Inline field title', value: 'Some value here', inline: true },
@@ -108,7 +192,7 @@ client.on('message', message => {
 			.setTimestamp()
 			.setFooter('Rahulio ' + version, 'https://imgur.com/12fKOZZ.jpg');
 		message.author.send(embed);
-
+		
 }	
 
 
@@ -122,15 +206,15 @@ client.on('message', message => {
 		if(args.length <= 0){message.channel.send(`Please input at least one term to ensure accurate Cartinese translation!`);}
 		else{
 			const lineOne = [`+*`,`ok !`,`+!:)`,`:black_heart:`,`*  *`,`!!`,`:butterfly:`,`:butterfly:`];
+			const lineTwo = [`:)`,`xo !`,`:(`,`slatt_!`,`*`,`#love *`,`ok !`, "_", "_", "_"];
 			const lineThree = [`++**`,`+:)`,`:broken_heart:`,`:mushroom::mushroom:`,`& *`,`:two_hearts:`,`lit` + `**!++`];
-			const lineTwo = [`:)`,`xo !`,`:(`,`slatt_!`,`*`,`#love *`,`ok !`, "_", "_", "_", "_"];
 			var temp1;
 			var temp2;
 			var temp3;
 
 			//line 1
-			var bfly = Math.floor(Math.random()*10);
-			if (bfly == 0){
+			var bfly = Math.floor(Math.random()*11);
+			if (bfly <= 1){
 				message.channel.send(lineOne[6] + lineOne[6] + lineOne[6] + `\n`);
 			}
 			else {
@@ -144,7 +228,15 @@ client.on('message', message => {
 			temp1 = lineTwo[Math.floor(Math.random() * lineTwo.length)];
 			temp2 = lineTwo[Math.floor(Math.random() * lineTwo.length)];
 			temp3 = lineTwo[Math.floor(Math.random() * lineTwo.length)];
-			message.channel.send(temp1 + ` ` + temp2 + ` ` + message.content.substring(8) + ` ` + temp3);		
+			var split1; var split2;
+			if (message.content.substring(8).length > 1){
+				split1 = message.content.substring(8).substring(0, Math.round(message.content.substring(8).length/2));
+				split2 = message.content.substring(8).substring(Math.round(message.content.substring(8).length/2));
+				message.channel.send(temp1 + ` ` + split1 + ` ` + temp2 + ` ` + split2 + ` ` + temp3)
+			}
+			else {
+				message.channel.send(temp1 + ` ` + temp2 + ` ` + message.content.substring(8) + ` ` + temp3 + ` ` + temp2);		
+			}
 
 			//line 3
 			temp1 = lineThree[Math.floor(Math.random() * lineThree.length)];
@@ -162,6 +254,6 @@ client.on('guildMemberAdd', member => {
 	if (!channel) return;
 
 	const emb = new Discord.MessageEmbed()
-		.setTitle(`Welcome to `+ guild.name + `, ${member}!\nPlease familiarize yourself with the server's rules.`);
+		.setTitle(`Welcome to `+ guild.name + `, ${member}!\nPlease familiarize yourself with the server's rules!`);
 	channel.send(emb);
 });
